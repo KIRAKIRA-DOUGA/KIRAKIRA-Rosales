fd1a9c646a1750ea6b90b228bfd5aef3c7cb9d63
cfdxkk <b2567240058@gmail.com>
2023-06-15 23:33:39 +0800

    :sparkles: :fire: 设置允许跨域（全部请求都允许）

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import router from './route/router'
 import Koa from 'koa'
 import fs from 'fs'
 import https from 'https'
+import cors from '@koa/cors'
 
 const SERVER_PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : 4000 // 从环境变量中获取端口号，如果没获取到，则使用4000
 
@@ -10,25 +11,19 @@ const serverEnv = process.env.SERVER_ENV
 
 const app = new Koa()
 
+app
+	.use(bodyParser())
+	.use(router.routes()) // 使用 koa-router
+	.use(router.allowedMethods()) // 所有路由中间件调用完成，ctx.status 仍为空或 404，程序自动丰富请求的响应头，方便 debug 或 handle
+	.use(cors()) // 允许跨域
+
+
 if (serverEnv && serverEnv !== 'dev') { // 非开发环境
-	app
-		.use(bodyParser())
-		.use(router.routes()) // 使用 koa-router
-		.use(router.allowedMethods()) // 所有路由中间件调用完成，ctx.status 仍为空或 404，程序自动丰富请求的响应头，方便 debug 或 handle
-		// .listen(SERVER_PORT) // 监听指定端口
-		
 	https.createServer({
 		key: fs.readFileSync('/usr/src/app/ssl/privkey.pem', 'utf8'),
 		cert: fs.readFileSync('/usr/src/app/ssl/fullchain.pem', 'utf8'),
 	}, app.callback()).listen(SERVER_PORT)
 } else { // 开发环境
 	app
-		.use(bodyParser())
-		.use(router.routes()) // 使用 koa-router
-		.use(router.allowedMethods()) // 所有路由中间件调用完成，ctx.status 仍为空或 404，程序自动丰富请求的响应头，方便 debug 或 handle
 		.listen(SERVER_PORT) // 监听指定端口
 }
-
-
-
-
