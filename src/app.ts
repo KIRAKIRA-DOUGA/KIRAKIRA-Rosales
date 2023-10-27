83985fce6d6e110953598bba51c6355a7b8930b2
cfdxkk <b2567240058@gmail.com>
2023-10-27 21:14:53 +0800

    :sparkles: 在开发环境和生产环境都使用 HTTPS（开发环境使用与前端一致的自签证书）

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,8 @@ import bodyParser from 'koa-bodyparser'
 import { connectMongoDBCluster } from './dbPool/DbClusterPool.js'
 import router from './route/router.js'
 
-const SERVER_PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : 9999 // 从环境变量中获取端口号，如果没获取到，则使用 9999
-
-const serverEnv = process.env.SERVER_ENV
+const SERVER_PORT = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : 9999 // 从环境变量中获取端口号，如果没获取到，则使用 9999
+const SERVER_ENV = process.env.SERVER_ENV
 
 const app = new Koa()
 
@@ -25,10 +24,12 @@ await connectMongoDBCluster().catch(error => {
 	process.exit()
 })
 
-if (serverEnv && serverEnv !== 'dev') { // 非开发环境，启用 https（需要提供 ssl 证书）
+if (SERVER_ENV && SERVER_ENV !== 'dev') { // dev 环境，使用自签名证书，非开发环境，从环境变量中读取证书
+	const SSL_KEY = process.env.SSL_KEY || ''
+	const SSL_CERT = process.env.SSL_CERT || ''
 	https.createServer({
-		key: fs.readFileSync('/usr/src/app/ssl/privkey.pem', 'utf8'),
-		cert: fs.readFileSync('/usr/src/app/ssl/fullchain.pem', 'utf8'),
+		key: SSL_KEY,
+		cert: SSL_CERT,
 	}, app.callback()).listen(SERVER_PORT)
 } else { // 开发环境，使用自签名证书
 	https.createServer({
