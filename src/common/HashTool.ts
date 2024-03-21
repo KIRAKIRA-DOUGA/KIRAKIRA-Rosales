252fa40e23e10d776f091f18e5b327002345b482
cfdxkk <b2567240058@gmail.com>
2024-03-21 22:49:54 +0800

    :fire: 清理无用代码

--- src/common/HashTool.ts
+++ src/common/HashTool.ts
@@ -1,7 +1,6 @@
 import bcrypt from 'bcrypt'
 
-const HASH_ROUND = 8 // Bcrypt Hash 轮次，数值越大越慢，越安全。
-
+const HASH_ROUND = 8 // Bcrypt Hash 轮次，数值越大越慢，越安全。 // WARN 千万不要改！
 /**
  * 使用 Bcrypt Hash 一个密码
  * @param password 原密码
@@ -20,46 +19,3 @@ export function hashPasswordSync(password: string): string {
 export function comparePasswordSync(passwordOrigin: string, passwordHash: string): boolean {
 	return bcrypt.compareSync(passwordOrigin, passwordHash)
 }
-
-
-// /**
-//  * 将输入的字符串进行简单的 SHA-256 哈希
-//  *
-//  * @param data 输入的字符串
-//  * @returns hash
-//  */
-// export const hashData = async (data: string): Promise<string> => {
-// 	if (data) {
-// 		const encoder = new TextEncoder()
-// 		const dataUint8Array = encoder.encode(data)
-// 		const hashBuffer = await crypto.subtle.digest('SHA-256', dataUint8Array)
-// 			.catch(e => {
-// 				console.error('something error in function hashData -> crypto.subtle.digest', e)
-// 			}) as Buffer
-// 		const hashArray = Array.from(new Uint8Array(hashBuffer))
-
-// 		return hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-// 	} else {
-// 		console.error('something error in function hashData, required data is empty')
-// 		return ''
-// 	}
-// }
-
-
-
-// const scrypt = util.promisify(crypto.scrypt)
-// /**
-//  * 生成加盐 hash
-//  *
-//  * @param data 被 hash 的字符串
-//  * @param salt 盐
-//  * @returns 加盐hash
-//  */
-// export const generateSaltedHash = async (data: string, salt: string): Promise<string> => {
-// 	const hash = await scrypt(data, salt, 64)
-// 		.catch(e => {
-// 			console.error('something error in function generateSaltedHash -> scrypt', e)
-// 		}) as Buffer
-// 	return hash.toString('hex')
-// }
-
