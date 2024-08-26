374a9472775e75b3f84a898ecdd472592c6a990a
Yasaitori <118044114+YasaitoriOvO@users.noreply.github.com>
2024-08-26 15:33:04 +0800

    Update src/controller/DanmakuControllerDto.d.ts
    Co-authored-by: Rantetsu Inori <56647156+otomad@users.noreply.github.com>

--- src/controller/DanmakuControllerDto.d.ts
+++ src/controller/DanmakuControllerDto.d.ts
@@ -6,7 +6,7 @@ type BasicDanmakuDto = {
 	videoId: number;
 	/** 非空 - 用户 UUID */
 	uuid: string;
-	/** 非空 用户 UID */
+	/** 非空 - 用户 UID */
 	uid: number;
 	/** 非空 - 弹幕发送的时机，单位：秒（支持小数） */
 	time: number;
