a759fd960804988d7f605836bb301aebefb29130
Aira Sakuranomiya <aira@aira.cafe>
2026-03-27 08:32:29 +0800

    🔥 移除不必要的附件发送

--- src/common/EmailTool.ts
+++ src/common/EmailTool.ts
@@ -102,11 +102,11 @@ export const sendMail = async (to: string, title: string, body: EmailBodyType) =
 				headers: { "Content-ID": "<background>" },
 			}, */
 			// 糟糕，许多邮箱不支持 CSS background-image 的内嵌图片显示！
-			{
+			/* {
 				path: resolve(import.meta.dirname, "../assets/images", "banner.png"),
 				type: "image/png",
 				headers: { "Content-ID": "<banner>" },
-			},
+			}, */
 			// #endregion
 		],
 	}
